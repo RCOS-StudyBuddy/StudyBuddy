71c763773148181cdc175d4bc73babfbeff89f39
MarinaEspinoza <espinm3@rpi.edu>
2015-06-18 14:23:17 -0400

    PLEASE WORK

---
+++ app/src/androidTest/java/studybuddy/github/com/rcos_studybuddyapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package studybuddy.github.com.rcos_studybuddyapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
